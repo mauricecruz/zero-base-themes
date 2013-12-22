3fc94d340fb98db5cf6163da0c8f32ac24f1bf35
Maurice <maurice.cruz@gmail.com>
2013-12-22 15:09:22 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+	/* Load Plugins */
+	
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
+
+	grunt.initConfig({
+		pkg: '<json:package.json>',		
+		less: {
+			stable: {
+				files: {					
+					'stable-theme-extension/styles.css': 'less/stable/build.less'		
+				}
+			},
+			canary: {
+				files: {									
+					'canary-theme-extension/styles.css': 'less/canary/build.less'				
+				}
+			}
+		},
+		watch: {
+			canary: {
+				files: ['less/canary/*.less','themes/*.less'],
+				tasks: ['less:canary']
+			},
+			stable: {
+				files: ['less/stable/*.less'],
+				tasks: ['less:stable']
+			},
+			
+		}
+
+	});
+	
+	grunt.registerTask('default', ['less']);
+};
