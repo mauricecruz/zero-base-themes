cbbce9d987fa3b8df31327e56698d4beac5e9cee
Maurice <maurice.cruz@gmail.com>
2014-10-17 15:04:54 -0400

    bump version of stable

--- theme-extension/devtools.js
+++ theme-extension/devtools.js
@@ -1,7 +1,7 @@
 var xhr = new XMLHttpRequest(),
 	stylesheet = 'stable.css';
 
-if (/Chrome\/(\d\d)/.exec(navigator.userAgent)[1] > 37) {
+if (/Chrome\/(\d\d)/.exec(navigator.userAgent)[1] > 38) {
 	stylesheet = 'canary.css'
 }
 
